343713308977523565e45950f5f3ed87fbfdec2e
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-16 18:47:22 +0100

    printed in page paragraph and its lenght

--- form.php
+++ form.php
@@ -1,4 +1,21 @@
 <?php
 $paragraph=$_GET['paragraph'];
 $badwords=$_GET['badwords']
+
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?= $paragraph ?></p>
+    <p><?= strlen(trim($paragraph))?></p>
+    
+</body>
+</html>
